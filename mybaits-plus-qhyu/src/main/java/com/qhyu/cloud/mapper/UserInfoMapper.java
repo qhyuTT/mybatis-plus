15229550b3a22787a8f726fb2175be4fdcc87ab9
yuqihai <yuqihai@travelsky.com>
2023-03-27 17:08:31 +0800

    mybatisplus缓存

--- mybaits-plus-qhyu/src/main/java/com/qhyu/cloud/mapper/UserInfoMapper.java
+++ mybaits-plus-qhyu/src/main/java/com/qhyu/cloud/mapper/UserInfoMapper.java
@@ -2,6 +2,7 @@ package com.qhyu.cloud.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.qhyu.cloud.model.SkyworthUser;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -18,6 +19,7 @@ import java.util.List;
  */
 // 按照我的理解这个@Mapper可以不写，到时候再看看
 @Mapper
+@CacheNamespace
 public interface UserInfoMapper extends BaseMapper<SkyworthUser> {
 
     List<SkyworthUser> getAll();
--- mybaits-plus-qhyu/src/main/java/com/qhyu/cloud/model/SkyworthUser.java
+++ mybaits-plus-qhyu/src/main/java/com/qhyu/cloud/model/SkyworthUser.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 
 /**
@@ -19,7 +20,7 @@ import java.util.Date;
  */
 @Data
 @TableName("skyworth_user")
-public class SkyworthUser {
+public class SkyworthUser implements Serializable {
 
     @TableId(value = "ID")
     private String id;
